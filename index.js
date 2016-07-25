d63adcbbbf1926f817e702350ec20099d6b993f1
James Pegg <jamescpegg@gmail.com>
2016-07-25 21:09:38 +0100

    Finished off basic theme

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 'use strict';
 
+var express = require('express');
+
 module.exports = function(app) {
   app.set('views', [
     process.cwd() + '/views/',
     __dirname + '/views/'
   ]);
+
+  app.use(express.static(__dirname + '/dist/'));
 
   app.locals.basedir = __dirname + '/views/';
 }
