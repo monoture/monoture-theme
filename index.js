bfc40e91d983cc21ce38011c2ebd44315306dd9a
James Pegg <jamescpegg@gmail.com>
2016-09-11 22:39:59 +0100

    Now using Monoture's registerTheme() method

--- index.js
+++ index.js
@@ -1,14 +1,5 @@
 'use strict';
 
-var express = require('express');
-
-module.exports = function(app) {
-  app.set('views', [
-    process.cwd() + '/views/',
-    __dirname + '/views/'
-  ]);
-
-  app.use(express.static(__dirname + '/dist/'));
-
-  app.locals.basedir = __dirname + '/views/';
+module.exports = function(monoture) {
+  monoture.registerTheme(__dirname + '/views/', __dirname + '/dist/');
 }
